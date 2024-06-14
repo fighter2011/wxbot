4b33473b4efa7d0630592a84a2e7e19fb8b8e143
吴秋林 <wuqiulin2011@qq.com>
2024-06-14 14:30:59 +0800

    feat(*): 添加构建docker脚本

---
+++ docker/build_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 检查是否提供了版本号参数
+if [ -z "$1" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+# 定义镜像名称和版本
+IMAGE_NAME="wxbot"
+VERSION="$1"
+LATEST="latest"
+
+# 创建并使用 Buildx builder
+docker buildx create --name multiarch-builder --use
+docker buildx inspect --bootstrap
+
+# 构建并推送多平台镜像
+#docker buildx build --platform linux/amd64,linux/arm64 \
+#    -t ${IMAGE_NAME}:${VERSION} \
+#    -t ${IMAGE_NAME}:${LATEST} \
+#    --load .
+# 构建并推送多平台镜像
+docker buildx build --platform linux/arm64 \
+    -t ${IMAGE_NAME}:${VERSION} \
+    -t ${IMAGE_NAME}:${LATEST} \
+    --load .
+
+# 清理 Buildx builder
+docker buildx rm multiarch-builder
